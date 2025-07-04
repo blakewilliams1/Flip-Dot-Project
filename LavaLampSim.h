809b327725995ae5c4ce33440c6f2804376aac3f
blakewilliams1 <blake@blakewilliams.org>
2025-07-03 19:55:23 -0700

    Adding negative metaballs to simulation
    I think they make it less obvious there's a pattern to how metaballs bounce and adds interesting temporary voids

--- LavaLampSim.h
+++ LavaLampSim.h
@@ -5,33 +5,40 @@
 #include "ControllerInput.h"
 
 typedef struct {
-  int x;
-  int y;
-  int dx;
-  int dy;
+  float x;
+  float y;
+  float dx;
+  float dy;
 } Ball;
 
 class LavaLampSim
 {
   public:
     LavaLampSim(FlipDotDriver& driver);
-    void maybeTickSimulation();
+    void maybeTickSimulation(CONTROLLER_INPUT controllerValue, bool wasPressed);
 
   private:
     FlipDotDriver& display;
     int lastSimulationTickTimestamp;
-    int maxSimulationHz = 10;
+    int maxSimulationHz = 7;
     const static int maxX = 56;
     const static int maxY = 28;
-    const static int numBalls = 8;
+    const static int numBalls = 10;
+    const static int numNegativeBalls = 3;
     constexpr static float radiusIn = 110;
-    constexpr static float radius = (radiusIn * radiusIn + (float)(maxX * maxY)) / 15000;//0.9112
-    const static int margin = 0;
+    constexpr static float radius = 0.9;//(radiusIn * radiusIn + (float)(maxX * maxY)) / 15000;//0.9112
+    // This allows the balls to bounce slightly out of bounds to improve the look.
+    const static int margin = -5;
     // Increase this value for thicker metaballs.
     constexpr static float pixelActivationThreshold = 0.0225;
     Ball balls[numBalls] = {};
+    Ball negativeBalls[numNegativeBalls] = {};
 
     void tickSimulation();
+    void resetDirections();
+    void simulateBall(Ball& ball);
+    float calculateDistSquared(Ball& ball, int x, int y);
+    void resetBallState(Ball& ball);
 };
 
 #endif // LAVALAMPSIM_H
